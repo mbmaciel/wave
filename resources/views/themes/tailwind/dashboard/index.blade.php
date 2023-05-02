6ab2571d5080c71b6adc54d834f06d2c2ae717e1
Mauricio Maciel <mbmaciel@outlook.com>
2023-05-02 12:05:35 -0300

    Update index.blade.php

--- resources/views/themes/tailwind/dashboard/index.blade.php
+++ resources/views/themes/tailwind/dashboard/index.blade.php
@@ -12,10 +12,10 @@
 				</div>
 				<div class="relative flex-1">
 	                <h3 class="text-lg font-medium leading-6 text-gray-700">
-	                    Welcome to your Dashboard
+	                    Bem vindo ao Painel Unobot
 	                </h3>
 	                <p class="text-sm leading-5 text-gray-500 mt">
-	                    Learn More Below
+	                    Operações binárias automatizadas
 	                </p>
 				</div>
 
